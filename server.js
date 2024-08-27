b12133169cb5042a883ac72a149b3d8141c8462d
gustavoaof <gustavoaraujoof@gmail.com>
2024-08-27 18:25:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const youtubedl = require('yt-dlp-exec'); 
 const fs = require('fs');
 const path = require('path');
-const { spawn } = require('child_process');
+const { spawn } = require('child_process');  
 const app = express();
 const port = process.env.PORT || 3001;
 
