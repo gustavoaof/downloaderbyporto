d31c91410679720baa71624397d8ffc2cd9d7790
gustavoaof <gustavoaraujoof@gmail.com>
2024-08-27 16:52:55 -0400

    Update script.js
    codigo errado

--- script.js
+++ script.js
@@ -1,107 +1,85 @@
-const express = require('express');
-const youtubedl = require('yt-dlp-exec'); 
-const fs = require('fs');
-const path = require('path');
-const app = express();
-const port = 3001;
+document.querySelectorAll('.format-button').forEach(button => {
+    button.addEventListener('click', function() {
+        document.querySelectorAll('.format-button').forEach(btn => btn.classList.remove('selected'));
+        this.classList.add('selected');
+        document.getElementById('selectedFormat').value = this.getAttribute('data-format');
+    });
+});
 
-// Caminho para o arquivo que armazena o contador de downloads
-const downloadCountFile = path.join(__dirname, 'downloads/count/texte.txt');
+document.getElementById('videoUrl').addEventListener('input', async function() {
+    const url = this.value;
+    const container = document.getElementById('container');
+    const errorMessage = document.getElementById('errorMessage');
+    const downloadButton = document.getElementById('downloadButton');
 
-// Inicializar o contador de downloads
-let downloadCount = 0;
+    const isValidUrl = isSupportedUrl(url);
 
-// Verificar se o arquivo do contador existe, se não, criar um
-if (fs.existsSync(downloadCountFile)) {
-    downloadCount = parseInt(fs.readFileSync(downloadCountFile, 'utf8')) || 0;
-} else {
-    fs.mkdirSync(path.dirname(downloadCountFile), { recursive: true });
-    fs.writeFileSync(downloadCountFile, '0');
+    if (isValidUrl) {
+        container.classList.remove('error'); 
+        errorMessage.style.display = 'none'; 
+        downloadButton.classList.remove('hidden'); 
+        try {
+            const videoInfo = await fetchVideoInfo(url);
+            if (videoInfo) {
+                document.getElementById('videoThumbnail').src = videoInfo.thumbnail;
+                document.getElementById('videoTitle').textContent = videoInfo.title;
+                const videoInfoElement = document.getElementById('videoInfo');
+                const bodyElement = document.body;
+
+                bodyElement.style.backgroundImage = `url(${videoInfo.thumbnail})`;
+                
+                videoInfoElement.style.display = 'block'; 
+                videoInfoElement.classList.add('show'); 
+                document.getElementById('container').classList.add('move-up');
+            }
+        } catch (error) {
+            console.error("Erro ao obter informações do vídeo:", error);
+        }
+    } else {
+        container.classList.add('error'); 
+        errorMessage.style.display = 'block'; 
+        downloadButton.classList.add('hidden'); 
+    }
+});
+
+function isSupportedUrl(url) {
+    const supportedSites = [
+        'youtube.com/watch', 'youtube.com/shorts', 'youtu.be'
+    ];
+    return supportedSites.some(site => url.includes(site));
 }
 
-// Middleware para servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'src/public')));
-
-app.get('/', (req, res) => {
-    const indexPath = path.join(__dirname, 'src/public', 'index.html');
-    res.sendFile(indexPath);
-});
-
-// Rota para obter o contador de downloads
-app.get('/downloads/count', (req, res) => {
-    res.json({ count: downloadCount });
-});
-
-app.get('/download', async (req, res) => {
-    const url = req.query.url;
-    const format = req.query.format;
-
-    console.log(`URL: ${url}`);
-    console.log(`Format: ${format}`);
-
-    if (!url || !format || não ['mp4', 'wav', 'mp3'].includes(format)) {
-        return res.status(400).send('Formato não suportado. Use mp4, wav ou mp3.');
-    }
-
-    try {
-        // Usando yt-dlp-exec para obter informações sobre o vídeo
-        const info = await youtubedl(url, {
-            dumpSingleJson: true,
-            noWarnings: true,
-            noCallHome: true
-        });
-
-        const title = info.title.replace(/[^a-zA-Z0-9 ]/g, '');
-        let outputFilename = `${title}.${format}`;
-        let outputPath = path.resolve(__dirname, 'downloads', outputFilename);
-
-        if (fs.existsSync(outputPath)) {
-            fs.unlinkSync(outputPath);
+function startDownload(event) {
+    event.preventDefault();
+    const downloadButton = document.getElementById('downloadButton');
+    downloadButton.disabled = true;
+    downloadButton.textContent = "Aguarde... 15";
+    document.getElementById('downloadForm').submit();
+    let countdown = 15;
+    const interval = setInterval(() => {
+        countdown -= 1;
+        downloadButton.textContent = `Aguarde... ${countdown}`;
+        if (countdown <= 0) {
+            clearInterval(interval);
+            downloadButton.disabled = false;
+            downloadButton.textContent = "Download";
         }
+    }, 1000);
+}
 
-        // Baixar o vídeo ou áudio com yt-dlp-exec
-        if (format === 'mp4') {
-            await youtubedl(url, {
-                format: 'bestvideo+bestaudio',
-                mergeOutputFormat: 'mp4',
-                output: outputPath
-            });
-        } else {
-            const tempPath = path.resolve(__dirname, 'downloads', 'download.m4a');
-            await youtubedl(url, {
-                format: 'bestaudio',
-                output: tempPath
-            });
+async function fetchVideoInfo(url) {
+    const videoId = extractVideoId(url);
+    if (!videoId) return null;
+    const apiUrl = `https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoId}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return {
+        title: data.title,
+        thumbnail: data.thumbnail_url
+    };
+}
 
-            const ffmpegCommand = spawn('ffmpeg', ['-i', tempPath, format === 'wav' ? outputPath : outputPath.replace(/\.wav$/, '.mp3'), '-y']);
-            ffmpegCommand.on('close', () => {
-                res.download(outputPath, outputFilename, (err) => {
-                    if (!err) {
-                        fs.unlinkSync(outputPath);
-                        fs.unlinkSync(tempPath);
-                        // Incrementar o contador de downloads
-                        downloadCount++;
-                        fs.writeFileSync(downloadCountFile, downloadCount.toString());
-                    }
-                });
-            });
-            return;
-        }
-
-        res.download(outputPath, outputFilename, (err) => {
-            if (!err) {
-                fs.unlinkSync(outputPath);
-                // Incrementar o contador de downloads
-                downloadCount++;
-                fs.writeFileSync(downloadCountFile, downloadCount.toString());
-            }
-        });
-    } catch (error) {
-        console.error(`Erro ao processar o download: ${error.message}`);
-        return res.status(500).send('Erro ao processar o download');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-});
+function extractVideoId(url) {
+    const videoIdMatch = url.match(/[?&]v=([^&#]*)|\/shorts\/([^\/?&]*)|youtu.be\/([^\/?&#]*)/);
+    return videoIdMatch ? (videoIdMatch[1] || videoIdMatch[2] || videoIdMatch[3]) : null;
+}
